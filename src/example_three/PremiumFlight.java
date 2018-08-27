e9e115bbdfd25f4b65ea69f863decb3b494a2139
Dieseled-UP <bourne.denis@gmail.com>
2018-08-27 02:38:14 +0100

    Created new class for Premium flight feature

---
+++ src/example_three/PremiumFlight.java
@@ -0,0 +1,22 @@
+package example_three;
+
+public class PremiumFlight extends Flight {
+
+	public PremiumFlight(int id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean addPassenger(Passenger passenger) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removePassenger(Passenger passenger) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
